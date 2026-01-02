7a8d8c5aa10a5e4d056fa5dfacbdc97d10518c7d
sergey <isergey207207@gmail.com>
2026-01-02 19:50:31 +0300

    Added 'copy' to codecs

--- src/app/ffmpeg_manager/codec.rs
+++ src/app/ffmpeg_manager/codec.rs
@@ -8,6 +8,7 @@ pub enum VideoCodec {
     HevcVaapi,
     Libsvtav1,
     Av1Vaapi,
+    Copy,
 }
 
 impl Display for VideoCodec {
@@ -18,6 +19,7 @@ impl Display for VideoCodec {
             VideoCodec::HevcVaapi => f.write_str("hevc_vaapi"),
             VideoCodec::Libsvtav1 => f.write_str("libsvtav1"),
             VideoCodec::Av1Vaapi => f.write_str("av1_vaapi"),
+            VideoCodec::Copy => f.write_str("copy"),
         }
     }
 }
@@ -31,12 +33,14 @@ impl Default for VideoCodec {
 #[derive(Debug, EnumIter, PartialEq, Eq, Clone)]
 pub enum AudioCodec {
     Libopus,
+    Copy,
 }
 
 impl Display for AudioCodec {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             AudioCodec::Libopus => f.write_str("libopus"),
+            AudioCodec::Copy => f.write_str("copy"),
         }
     }
 }
@@ -51,6 +55,7 @@ impl Default for AudioCodec {
 pub enum SubtitleCodec {
     Srt,
     Ass,
+    Copy,
 }
 
 impl Display for SubtitleCodec {
@@ -58,6 +63,7 @@ impl Display for SubtitleCodec {
         match self {
             SubtitleCodec::Srt => f.write_str("srt"),
             SubtitleCodec::Ass => f.write_str("ass"),
+            SubtitleCodec::Copy => f.write_str("copy"),
         }
     }
 }
@@ -73,6 +79,7 @@ pub enum PixelFormat {
     Yuv420p,
     Nv10,
     Yuv420p10le,
+    Copy,
 }
 
 impl Display for PixelFormat {
@@ -81,6 +88,7 @@ impl Display for PixelFormat {
             PixelFormat::Yuv420p => f.write_str("yuv420p"),
             PixelFormat::Nv10 => f.write_str("nv10"),
             PixelFormat::Yuv420p10le => f.write_str("yuv420p10le"),
+            PixelFormat::Copy => f.write_str("copy"),
         }
     }
 }
